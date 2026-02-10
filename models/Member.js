45fa2459ed3ee1123d553a80357732dae85a837e
Tata <142669125+Dotaka123@users.noreply.github.com>
2026-02-10 11:45:50 +0300

    Update Member.js

--- models/Member.js
+++ models/Member.js
@@ -82,7 +82,7 @@ const memberSchema = new mongoose.Schema({
 // Index sparse - ignore les valeurs null
 memberSchema.index(
   { email: 1 }, 
-  { unique: false, sparse: true, partialFilterExpression: { email: { $type: "string" } } }
+  { unique: true, sparse: true, partialFilterExpression: { email: { $type: "string" } } }
 );
 
 memberSchema.pre('save', function(next) {
